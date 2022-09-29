2f0ef5d4e3e5312f62fb8f5ee9dffd68e3579a53
achyut kadam <achyut.k@acldigital.com>
2022-09-29 11:59:35 +0530

    learn variables

---
+++ variable.py
@@ -0,0 +1,42 @@
+x = 5
+y = "John"
+print(x)
+print(y)
+
+x = str(3)    # x will be '3'
+y = int(4907)    # y will be 3
+z = float(3.14)  # z will be 3.0
+
+a = float(y) # convert int into float
+b = int(z) # convert float into int
+c = complex(x) # convert int into complex
+print('+++++++++++++++++++++++')
+print(a)
+print(b)
+print(c)
+print('+++++++++++++++++++++++')
+
+# print(type(x))
+# print(type(y))
+# print(type(z))
+
+x = 1j
+print(x)
+x = ["apple", "banana", "cherry"]
+print(x)
+x = ("apple", "banana", "cherry")
+print(x)
+x = range(2,6)
+print(x)
+x = b"kadam"
+print(x)
+x = bool(-5)
+print(x)
+x = bytes(5)
+print(x)
+x = bytearray(5)
+print(x)
+x = memoryview(bytes(5))
+print(x)
+x = float(20.5)
+print(type(x))
